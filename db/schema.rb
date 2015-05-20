d1c2411bf1e66da0b029b11ed841b7cd30ce4ce1
Matthew Thompson <matt@mattathompson.org>
2015-05-20 19:57:49 -0400

    migrated

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150518034147) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "images", force: :cascade do |t|
     t.string   "description"
     t.string   "attachment_file_name"
@@ -47,7 +50,7 @@ ActiveRecord::Schema.define(version: 20150518034147) do
     t.string   "last_name"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
